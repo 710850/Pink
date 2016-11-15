45f0a9b309c7c6610d354fb52b7edffd876b10cb
710850 <710850@gmail.com>
2016-11-16 00:27:55 +0400

    menu button is ready

--- js/common.js
+++ js/common.js
@@ -1,27 +1,5 @@
-var data = { updates: [
-    {
-        name: 'Jane Doe',
-        update: 'Just Made my Breakfaast',
-        from: 'Web',
-        location: 'Canada'
-    },
-    {
-        name: 'John Doe',
-        update: 'What is going on with the weather?',
-        from: 'Phone'
-    }
-]};
-var source = $('#template').html();
-var template = Handlebars.compile(source);
-$('.updates').append(template(data));
-
-
-
-/*(function (){
-    $.ajax({
-        url: 'js/data.json'
-    }).done(function (data) {
-        var  json = JSON.parse(data);
-        console.log(json);
+$(document).ready(function(){
+    $(".main-header__icon-menu").click(function () {
+        $(".main-header__navigation-list").slideToggle(500);
     });
-})()*/
+});
